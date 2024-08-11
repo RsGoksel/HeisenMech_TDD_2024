a18db416729f2c0be4c3b7c089f9eeb754dd8870
Göksel Gündüz <80707238+RsGoksel@users.noreply.github.com>
2024-08-11 16:09:59 +0300

    Update Tools.py

--- Tools.py
+++ Tools.py
@@ -1,12 +1,16 @@
-import pytesseract
-from PyPDF2 import PdfReader, PdfWriter
-from pdf2image import convert_from_path
-import numpy as np
+""" Author: RsMech - 12.06.2024 : Written for Teknofest2024_TDDİ """
+
 import cv2
 import re
 import json
 import os
 import glob
+import pytesseract
+
+import numpy as np
+
+from PyPDF2    import PdfReader, PdfWriter
+from pdf2image import convert_from_path
 
 def preprocess_image(image):
     
@@ -109,33 +113,8 @@ def extract_data(text):
 
     return dataset
 
-pattern = re.compile(r"Soru özeti_\d+: (.*?)\nParagraf_\d+: (.*?)\nSoru_\d+: (.*?)(?=\n\Soru özeti_\d+|$)", re.DOTALL)
 
-def extract_paragraf(text):
-    
-    matches = pattern.findall(text)
-    
-    data = []
-    
-    for match in matches:
-        
-        soru_ozeti = match[0].strip()
-        paragraf = match[1].strip()
-        soru = match[2].strip()
-        
-        entry = {
-            "soru_ozeti": soru_ozeti,
-            "paragraf": paragraf,
-            "soru": soru
-        }
-        
-        # Add the dictionary to the list
-        data.append(entry)
-        
-    return data
-
-pattern_ops = re.compile(r"Paragraf_\d+: (.*?)\nSoru_\d+: (.*?)\nŞıklar_\d+: (.*?)(?=\n\S|$)", re.DOTALL)
-             
+          
 
 def extract_opsiyon(text):
     matches = pattern_ops.findall(text)
@@ -156,7 +135,6 @@ def extract_opsiyon(text):
         data.append(entry)
         
     return data
-    
     
 def create_JSON_training_dataset(Json_folder_path, output_name, output_path='./', verbose=False):
     """
@@ -194,147 +172,8 @@ def create_JSON_training_dataset(Json_folder_path, output_name, output_path='./'
 
     if verbose:
         print(f"Dataset created and saved to {output_path + output_name}")
-
-alternates = [
-    " bu kelimeleri kullanarak bir paragraf sorusu yaz: ",
-    " bu kelimeler ile bir paragraf sorusu sor",
-    " bunlar ile bir paragraf sorusu sor",
-    " konularından bir paragraf sorusu yaz",
-    " bu kelimelerden bir paragraf sorusu sor",
-    " bu kelimeleri kullanarak Türkçe paragraf sorusu sor",
-    " bir paragraf sorusu yaz",
-    " : bu ifadeler ile bir paragraf sorusu oluştur",
-    " bu anahtar kelimeler ile bir paragraf sorusu yaz",
-    " : bu anahtar sözcükleri kullanarak bir paragraf sorusu oluştur",
-    " bu kelimelerden yola çıkarak bir paragraf sorusu sor",
-    " : bu kelimeleri kullanarak bir paragraf yaz ve bir soru sor",
-    " bu kelimelerden bir paragraf oluştur ve soruyu ekle",
-    " : bu kelimeleri kullanarak kapsamlı bir paragraf sorusu yaz",
-    " bu terimleri kullanarak bir paragraf sorusu oluştur",
-    " : verilen kelimelerle bir paragraf sorusu hazırla",
-    " bu sözcükleri kullanarak bir paragraf sorusu yaz",
-    " : bu ifadelerden bir paragraf sorusu oluştur",
-    " bu terimlerle bir paragraf sorusu hazırla",
-    " bu anahtar sözcüklerle bir paragraf sorusu yaz",
-]
-
+        
 import random
-
-def create_Paragraf_JSON_training_dataset(Json_folder_path, output_path, verbose=False):
-    """
-    JSON belgeleri içeren klasor yolu alarak tum json dosyalarından bir dataset JSON olusturur"""
-    dataset = []
-
-    # Traverse all files in the given folder
-    for filename in os.listdir(Json_folder_path):
-        if filename.endswith('.json'):
-            file_path = os.path.join(Json_folder_path, filename)
-            
-            # Read the JSON file
-            with open(file_path, 'r', encoding='utf-8') as file:
-                try:
-                    data = json.load(file)
-                except json.JSONDecodeError as e:
-                    print(f"Error decoding JSON from file {file_path}: {e}")
-                    continue
-
-                # Extract instruction and response
-                for item in data:
-                    instruction = item.get('soru_ozeti', '').strip()
-                    input = item.get('paragraf', '').strip()
-                    output = item.get('soru', '').strip()
-                
-                    if instruction and input and output:
-                        
-                        dataset.append({
-                            #"input": instruction,  
-                            'instruction': instruction + random.choice(alternates),
-                            "output": "Paragraf: " + input + 'Soru: ' + output
-                            
-                        })
-                    else:
-                        instruction = item.get('Soru özeti', '').strip()
-                        input = item.get('Paragraf', '').strip()
-                        output = item.get('Soru', '').strip()
-                        dataset.append({
-                            #"input": instruction,  
-                            'instruction': instruction + random.choice(alternates),
-                            "output": "Paragraf: " + input + 'Soru: ' + output
-                            
-                        })
-    
-    with open(output_path, 'w', encoding='utf-8') as out_file:
-        
-        json.dump(dataset, out_file, ensure_ascii=False, indent=4)
-
-    if verbose:
-        print(f"Dataset created and saved to {output_path}")
-
-alternate_options = [
-    " bu paragraf ve soruyu ele alarak 5 adet opsiyon yaz. ",
-    " verilen paragraftan ve sorusundan yola çıkarak 5 adet opsiyon yaz.",
-    " aşağıdaki paragraf ve soruya dayanarak 5 seçenek oluştur.",
-    " bu paragrafı ve soruyu kullanarak 5 farklı opsiyon yaz.",
-    " verilen paragraf ve soruya göre 5 seçenek oluştur.",
-    " aşağıdaki paragraftan ve sorudan 5 opsiyon üret.",
-    " bu paragraf ve soru için 5 farklı seçenek yaz.",
-    " verilen paragraf ve soru çerçevesinde 5 alternatif opsiyon yaz.",
-    " bu paragraf ve sorudan hareketle 5 seçenek yaz.",
-    " aşağıdaki paragraf ve soruya dayanarak 5 farklı seçenek oluştur.",
-    " bu metin ve soruyu kullanarak 5 adet seçenek yaz.",
-    " paragrafı ve soruyu kullanarak 5 farklı opsiyon üret.",
-    " aşağıdaki paragraf ve soru için 5 alternatif seçenek oluştur.",
-    " verilen metin ve sorudan 5 farklı seçenek yaz.",
-    " bu paragraf ve sorudan yola çıkarak 5 farklı opsiyon oluştur.",
-    " verilen paragraf ve soruyu kullanarak 5 adet opsiyon üret.",
-    " bu paragraf ve soruya dayanarak 5 adet seçenek oluştur.",
-    " aşağıdaki metin ve soru üzerinden 5 farklı opsiyon yaz.",
-    " bu paragraf ve sorudan yola çıkarak 5 farklı seçenek oluştur.",
-    " verilen paragraf ve soruyu kullanarak 5 alternatif opsiyon yaz."
-]
-
-def create_opsiyon_JSON_training_dataset(Json_folder_path, output_path, verbose=False):
-    """
-    JSON belgeleri içeren klasor yolu alarak tum json dosyalarından bir dataset JSON olusturur"""
-    dataset = []
-
-    # Traverse all files in the given folder
-    for filename in os.listdir(Json_folder_path):
-        if filename.endswith('.json'):
-            file_path = os.path.join(Json_folder_path, filename)
-            
-            # Read the JSON file
-            with open(file_path, 'r', encoding='utf-8') as file:
-                try:
-                    data = json.load(file)
-                except json.JSONDecodeError as e:
-                    print(f"Error decoding JSON from file {file_path}: {e}")
-                    continue
-
-                # Extract instruction and response 
-                # paragraf   #soru   #opsiyonlar
-                
-                for item in data:
-                    instruction = item.get('paragraf', '').strip() + item.get('soru', '').strip()  
-                    
-                    output = item.get('opsiyonlar', '').strip()
-                
-                    if instruction and input and output:
-                        
-                        dataset.append({
-                            'instruction': instruction + random.choice(alternate_options),
-                            "output": "Şıklar: " + output
-                            
-                        })
-                    
-    
-    with open(output_path, 'w', encoding='utf-8') as out_file:
-        
-        json.dump(dataset, out_file, ensure_ascii=False, indent=4)
-
-    if verbose:
-        print(f"Dataset created and saved to {output_path}")
-
 
 def JSON_merge(directory_path, output_path):
     """
@@ -356,37 +195,3 @@ def JSON_merge(directory_path, output_path):
 
     print(f"Merged JSON saved to {output_path}")
 
-
-def text_to_json(file_path):
-    """
-    Text dolu paragraf folder'ını alır ve hepsini json yapar.
-    """
-    
-    pattern = re.compile(r'Soru özeti_(\d+): (.*?)\nParagraf_\1: (.*?)\nSoru_\1: (.*?)\n', re.DOTALL)
-    
-    # Function to convert text to JSON
-    def convert_text_to_json(file_path):
-        with open(file_path, 'r', encoding='utf-8') as file:
-            content = file.read()
-    
-        matches = pattern.findall(content)
-    
-        data = []
-        for match in matches:
-            data.append({
-                'Soru özeti': match[1],
-                'Paragraf': match[2],
-                'Soru': match[3]
-            })
-    
-        json_path = file_path.replace('.txt', '.json')
-        with open(json_path, 'w', encoding='utf-8') as json_file:
-            json.dump(data, json_file, ensure_ascii=False, indent=4)
-    
-    # Traverse through the folder
-    for filename in os.listdir(folder_path):
-        if filename.endswith('.txt'):
-            file_path = os.path.join(folder_path, filename)
-            convert_text_to_json(file_path)
-    
-    print('Conversion completed.')
